d4445ce11c736c771f84a7ec0be073d91a3893de
Nadezhda <nadaya-beketova@yandex.ru>
2022-10-27 16:54:38 +0300

    Удаление пробелов

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    min=Math.ceil(min);
+    max=Math.floor(max);
+    return Math.floor(Math.random()*(max-min+1))+min; //Максимум и минимум включаются
   }
  // Функция 2.2
- function getRandomArbitrary(min, max, maxDigits = 0) {
-    if (min > max || min < 0 || max <= 0) {
+ function getRandomArbitrary(min, max, maxDigits=0) {
+    if (min>max || min<0 || max<=0) {
       return ('Задан неверный диапазон! Укажите другие числа.');
     }
-    const digitsDegree = 10 ** maxDigits;
-    return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+    const digitsDegree=10**maxDigits;
+    return ~~((Math.random()*(max - min)+min)*digitsDegree) / digitsDegree;
   }
   getRandomIntInclusive(
     2,
