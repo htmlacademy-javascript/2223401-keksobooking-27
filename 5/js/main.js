6dfbff221eab51c8c7c6eb9f06d87189c241e940
Keks <keks@htmlacademy.ru>
2022-11-02 05:47:46 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,4 +1,4 @@
-const avatars_count = 10;
+const count = 10;
 const title = [
   'Уютный номер у моря',
   'Горячее предложение месяца',
@@ -36,11 +36,6 @@ const photos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const location = [
-  'lat',
-  'lng',
-];
-// Функции нахождения случайных элементов 
 const getRandomPositiveInteger = (a, b) => {
   if (a < 0 || b < 0) {
     return NaN;
@@ -58,32 +53,33 @@ function getRandomPositiveFloat(a, b, digits = 1) {
   const upper = Math.max(a, b);
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
-};
+}
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-// Фугкция создания ссылки на изображение (аватарку)
+// Функция создания ссылки на изображение (аватарку)
 const createAuthor = () => {
-  if (getRandomPositiveInteger(1, avatars_count) < avatars_count) {
-    return `img/avatars/users0${getRandomPositiveInteger(1, avatars_count)}.png`;
+  if (getRandomPositiveInteger(1, count) < count) {
+    return `img/avatars/users0${getRandomPositiveInteger(1, count)}.png`;
   }
   return {
-    avatar: `img/avatars/users${getRandomPositiveInteger(1, avatars_count)}.png`,
-  }
+    avatar: `img/avatars/users${getRandomPositiveInteger(1, count)}.png`,
+  };
 };
+// Функция генерации features, массив строк — массив случайной длины из значений
 const createFeatures = () =>
   Array.from({ length: getRandomPositiveInteger(1, 2, 3, 4, 5, 6) }, () =>
     getRandomArrayElement(features)
   ).join('');
 // Функция создания одного объявления
 const createAdvert = () => {
-  const lat_x = getRandomPositiveFloat(48.00000, 36.56000, 6);
-  const lng_y = getRandomPositiveFloat(54.00000, 39.48000, 6);
+  const randomx = getRandomPositiveFloat(48.00000, 36.56000, 6);
+  const randomy = getRandomPositiveFloat(54.00000, 39.48000, 6);
   return {
     author: {
       avatar: createAuthor(),
     },
     offer: {
       title: getRandomArrayElement(title),
-      address: `${lat_x}, ${lng_y}`,
+      address: `${randomx}, ${randomy}`,
       price: getRandomPositiveInteger(1000, 100000000),
       type: getRandomArrayElement(type),
       rooms: getRandomPositiveInteger(1, 15),
@@ -92,19 +88,16 @@ const createAdvert = () => {
       checkout: getRandomArrayElement(checkin),
       features: createFeatures(),
       description: getRandomArrayElement(description),
-      photos: Array.from({ length: getRandomPositiveInteger(0, 3) },
-        (_, photoIndex) => createPhotos(photoIndex + 1)),
+      photos: Array.from({ length: getRandomPositiveInteger(1, 2) }, () =>
+        getRandomArrayElement(photos)
+      ).join(''),
     },
     location: {
-      lat: lat_x,
-      lng: lng_y,
+      lat: randomx,
+      lng: randomy,
     },
   };
 };
-
 // Функция генерации 10-ти случайных объявлений
-const getAdverts = () =>
-  Array.from({ length: avatars_count }, (_, advertIndex) =>
-    createAdvert(advertIndex + 1)
-  );
+const getAdverts = new Array(count).fill(null).map(() => createAdvert());
 getAdverts();
